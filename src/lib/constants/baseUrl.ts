103d0df573d457973105003ba779a2a873b9be00
unknown <mck0391@gmail.com>
2025-08-01 18:30:45 +0100

    dev commit v0.0.7

--- src/lib/constants/baseUrl.ts
+++ src/lib/constants/baseUrl.ts
@@ -5,6 +5,8 @@
 /* export const baseUrl = 'http://localhost:4000/api'; */
 /* export const baseUrl = '/api'; */
 
+
+
 export const baseUrl = import.meta.env.VITE_API_URL 
   ? `${import.meta.env.VITE_API_URL}/api`
   : '/api';
